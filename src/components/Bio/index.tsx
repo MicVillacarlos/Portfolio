1891371c96094d0ffd2e09f4b42d8d698d8562ef
MicVillacarlos <micvillacarlos@gmail.com>
2022-03-21 04:35:53 +0800

    change about page

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -10,7 +10,7 @@ export const Bio = () =>{
             <h1 data-aos="fade-right" data-aos-duration="1000">Michael Villacarlos makes Websites</h1> <br/>
             <p  data-aos="fade-right" data-aos-duration="3000">
             Mic is trying his best to keep his life together 🤞🤔. That's why he is always on his laptop, practicing his skills in coding and design. 
-            <br/>
+            <br/><br/>
             With that said, he consistently aims to achieve the best and most efficient result on each project that he tackles. 
             </p>
             <br/>
