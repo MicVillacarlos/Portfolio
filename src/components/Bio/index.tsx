de5bdd2beb2bc4a79166dfc29f601a862c6cc8ee
MicVillacarlos <micvillacarlos@gmail.com>
2022-03-30 00:27:29 +0800

    change Bio

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -9,7 +9,7 @@ export const Bio = () =>{
             <BioTextContainer >
             <h1 data-aos="fade-right" data-aos-duration="1000">I make creative and efficient Websites</h1><br/>
             <p  data-aos="fade-right" data-aos-duration="3000">
-            I'm a passionate developer based in Cebu City, Philippines. I love creating and designing innovative solutions to challenging problems. I consistently aim to achieve the best and most efficient result on each project that I tackle.
+            I'm a passionate developer based in Cebu City, Philippines. I consistently aim to achieve the best and most efficient result on each project that I tackle.
             <br/><br/>
             Aside from coding, I am also passionate about music. I love creating music with my band 🎹. I also enjoy outdoor activities such as cycling, running and hiking. 
             </p>
