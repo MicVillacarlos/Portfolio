e75b4556606ab9dbdee6960c299e19eeebce9b50
Michael <mike.villacarlos@symph.co>
2022-08-10 20:33:07 +0800

    fix form validation

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,66 +1,87 @@
-import { Container,Form,TitleAndButtonContainer} from './style'
-import emailjs from 'emailjs-com';
-import { useState } from 'react';
-import { Curly } from '../Curly'
-
+import { Container, Form, TitleAndButtonContainer, Error } from "./style";
+import emailjs from "emailjs-com";
+import { useState } from "react";
+import { Curly } from "../Curly";
 
 export const Contact = () => {
-const [senderEmail,setSenderEmail] = useState('')
-const [subject, setSubject] = useState('')
-const [message,setMessage] = useState('')
-const [isSent,setSent] = useState(false)
+  const [senderEmail, setSenderEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const [isSent, setSent] = useState(false);
 
-
-  const onSubmit =(e:React.FormEvent)=>{
-    e.preventDefault()
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
 
     let templateParams = {
       senderEmail: senderEmail,
       subject: subject,
-      message: message
-      };
-   
-    emailjs.send("service_hvulxqr","template_wqufufj", templateParams,
-    "m-m1mHmXoS2R-8-H5")
-      .then(function(response) {
-         console.log('SUCCESS!', response.status, response.text);
-      }, function(error) {
-         console.log('FAILED...', error);
-      });
-    
-    setSent(true)
-    setSenderEmail('');
-    setSubject('');
-    setMessage('');
-  }
+      message: message,
+    };
+
+    if (senderEmail === "") {
+      setError("Email should not be Empty!");
+    } else if (subject === "") {
+      setError("Subject should not be Empty!");
+    } else if (message === "") {
+      setError("Message should not be Empty!");
+    } else {
+      emailjs
+        .send(
+          "service_hvulxqr",
+          "template_wqufufj",
+          templateParams,
+          "m-m1mHmXoS2R-8-H5"
+        )
+        .then(
+          function (response) {
+            console.log("SUCCESS!", response.status, response.text);
+            setSent(true);
+            setSenderEmail("");
+            setSubject("");
+            setMessage("");
+            setError('');
+          },
+          function (error) {
+            console.log("FAILED...", error);
+          }
+        );
+    }
+  };
 
   return (
     <div>
-      <Curly/>
+      <Curly />
       <Container>
-        <div data-aos="zoom-in-up" id='contact'>
+        <div data-aos="zoom-in-up" id="contact">
           <Form onSubmit={onSubmit}>
             <TitleAndButtonContainer>
               <h3>Don't be shy, say Hi! 😀</h3>
             </TitleAndButtonContainer>
-              <label>Your Email</label>
-                <input 
-                value={senderEmail} 
-                onChange={(e)=>setSenderEmail(e.target.value)}/>
-              <label>Subject</label>
-                <input 
-                value={subject} 
-                onChange={(e)=>setSubject(e.target.value)}/>
-              <label>Message</label>
-                <textarea 
-                value={message} 
-                onChange={(e)=>setMessage(e.target.value)}/>
+            <label>Your Email</label>
+            <input
+              value={senderEmail}
+              onChange={(e) => setSenderEmail(e.target.value)}
+            />
+            <label>Subject</label>
+            <input
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+            <label>Message</label>
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+
+            <Error>{error}</Error>
+
             <TitleAndButtonContainer>
-              {isSent? "Message sent " : <button>Send</button>}
+              <button>Send</button>
             </TitleAndButtonContainer>
           </Form>
         </div>
-      </Container> 
-    </div>   
+      </Container>
+    </div>
   );
 };
--- src/components/Contact/style.tsx
+++ src/components/Contact/style.tsx
@@ -1,71 +1,74 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
-    height: 40vh;
+  height: 40vh;
+  padding: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 768px) {
     padding: 3em;
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    @media (max-width: 768px) {
-        padding:3em;
-    }
-`
+  }
+`;
 export const TitleAndButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: space-evenly;
+  justify-content: space-evenly;
+  gap: 0.5em;
+
+  border: ${(props) => props.theme.color.primary} 1px solid;
+  border-radius: 30px;
+  padding: 2em;
+  color: ${(props) => props.theme.color.primary};
+  min-width: 45vw;
+  font-family: ${(props) => props.theme.font.secondary};
+  h3 {
+    font-family: ${(props) => props.theme.font.primary};
+    text-align: center;
+  }
+
+  input {
+    font-family: ${(props) => props.theme.font.secondary};
+    border: ${(props) => props.theme.color.primary} 1px solid;
+    color: ${(props) => props.theme.color.primary};
+    height: 20px;
+  }
+  textarea {
+    font-family: ${(props) => props.theme.font.secondary};
+    height: 50px;
+    border: ${(props) => props.theme.color.primary} 1px solid;
+    resize: none;
+    color: ${(props) => props.theme.color.primary};
+  }
+  button {
+    font-family: ${(props) => props.theme.font.secondary};
+    border: ${(props) => props.theme.color.primary} 1px solid;
+    border-radius: 5px;
+    width: 4em;
+    color: ${(props) => props.theme.color.primary};
+    padding: 0.5em;
     display: flex;
     align-items: center;
     justify-content: center;
-`
-export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: space-evenly;
-    justify-content: space-evenly;
-    gap:0.5em;
 
-    border:${(props)=>props.theme.color.primary} 1px solid;
-    border-radius: 30px;
-    padding: 2em;
-    color: ${(props)=>props.theme.color.primary};
-    min-width: 45vw;
-    font-family: ${(props)=>props.theme.font.secondary};
-    h3{
-        font-family: ${(props)=>props.theme.font.primary};
-        text-align: center;
+    &:hover {
+      cursor: pointer;
+      background-color: ${(props) => props.theme.color.primary};
+      color: black;
     }
+    &:active {
+      transform: translateY(5%);
+      color: white;
+    }
+  }
+`;
 
-    input {
-        font-family: ${(props)=>props.theme.font.secondary};
-        border:${(props)=>props.theme.color.primary} 1px solid;
-        color:${(props)=>props.theme.color.primary};
-        height: 20px;
-    }
-    textarea {
-        font-family: ${(props)=>props.theme.font.secondary};
-        height: 50px;
-        border:${(props)=>props.theme.color.primary} 1px solid;
-        resize: none;
-        color:${(props)=>props.theme.color.primary};
-    }
-    button {
-        font-family: ${(props)=>props.theme.font.secondary} ; 
-        border:${(props)=>props.theme.color.primary} 1px solid;
-        border-radius: 5px;
-        width: 4em;
-        color: ${(props)=>props.theme.color.primary};
-        padding: 0.5em;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        &:hover{
-            cursor:pointer;
-            background-color: ${(props)=>props.theme.color.primary};
-            color:black;
-        }
-        &:active{
-            transform: translateY(5%);
-            color:white
-        }
-    }
+export const Error = styled.p`
+    color: #e45454
 `
