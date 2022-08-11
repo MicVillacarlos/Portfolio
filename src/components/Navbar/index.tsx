adfb658343a9f319476c48fd7fd24cffb84d215e
Michael <mike.villacarlos@symph.co>
2022-08-12 01:08:14 +0800

    update cv

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,35 +4,39 @@ import logoPic from '../../assets/logopic.png';
 import { Link } from 'react-scroll'
 
 export const Navbar= () => {
-    return(
-        <Container>
-            <LogoPic>
-                <img src={logoPic} alt="logo pic"/>
-            </LogoPic>
-            <ContainerNavItems>
-                <NavbarItem>
-                    <Link to='about' spy={true} smooth={true} duration={800}>
-                        ABOUT
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link to='works' spy={true} smooth={true} duration={800}>
-                        WORKS
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <a href="https://drive.google.com/file/d/1kh-grBqlk1zZGnQwXsREFz-yxKGsqjja/view?usp=sharing" target="_blank" rel="noreferrer"> 
-                    RESUME 
-                    </a>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link to='contact' spy={true} smooth={true} duration={800}>
-                        CONTACT
-                    </Link>
-                </NavbarItem>
-            </ContainerNavItems>
-        </Container>
-    )
+    return (
+      <Container>
+        <LogoPic>
+          <img src={logoPic} alt="logo pic" />
+        </LogoPic>
+        <ContainerNavItems>
+          <NavbarItem>
+            <Link to="about" spy={true} smooth={true} duration={800}>
+              ABOUT
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link to="works" spy={true} smooth={true} duration={800}>
+              WORKS
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <a
+              href="https://drive.google.com/file/d/1sODF9fss1-JJ5mXLG6fi2y87DprqwJdY/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
+            >
+              RESUME
+            </a>
+          </NavbarItem>
+          <NavbarItem>
+            <Link to="contact" spy={true} smooth={true} duration={800}>
+              CONTACT
+            </Link>
+          </NavbarItem>
+        </ContainerNavItems>
+      </Container>
+    );
 }
 
 
